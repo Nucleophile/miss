c4d8f9639d2ec5433db49dab2e6cd937685264a5
Zanuda <zanuda.brat.zarazy@gmail.com>
2024-08-09 04:55:51 +0300

    Added html-bundler-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,63 +1,42 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const svgToMiniDataURI = require("mini-svg-data-uri");
-const Pug = require("pug");
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: ["./src/index.js", "./src/styles.scss"]
-  },
   output: {
-    filename: "scripts/[name].min.js",
     path: path.resolve(__dirname, "dist"),
     clean: true
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.pug"
-    }),
-    new HtmlWebpackInlineSVGPlugin({
-      inlineAll: true
+    new HtmlBundlerPlugin({
+      preprocessor: "pug",
+      entry: {
+        index: './src/index.pug',
+      },
+      js: {
+        filename: 'scripts/main.min.js',
+      },
+      css: {
+        filename: 'styles/main.min.css',
+      },
     }),
     new webpack.SourceMapDevToolPlugin({
       filename: "[file].map[query]"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles/[name].min.css"
     })
   ],
   module: {
     rules: [
       {
-        test: /\.pug$/i,
-        loader: "html-loader",
-        options: {
-          preprocessor: (content) => {
-            return Pug.compile(content, {
-              basedir: path.resolve(__dirname, "src")
-            })();
-          }
-        }
-      },
-      {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: ["css-loader", "sass-loader"]
       },
       {
         test: /\.(png|img|svg|jpg|jpeg|gif|ico)$/i,
         oneOf: [
           {
             resourceQuery: /inline/,
-            type: "asset/inline",
-            generator: {
-              dataUrl: (content) => {
-                content = content.toString();
-                return svgToMiniDataURI(content);
-              }
-            }
+            type: "asset/inline"
           },
           {
             type: "asset/resource",
